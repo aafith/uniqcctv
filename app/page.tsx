9d68763bea7fe8266b908f39a78844e17e3a93df
Mohamed Aafith <117595648+aafith@users.noreply.github.com>
2026-09-12 14:39:07 +0530

    Revert "hero update scroll animation"

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import Header from "@/components/layouts/Header";
-import Hero from "@/components/home/Hero";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <Hero />
     </>
   );
 }
